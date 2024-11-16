aa722e7a2d40cbac3385dc2bff8ed94e849ec2c1
Luis Zancanela <luisczdidi@gmail.com>
2024-11-16 16:40:36 -0300

    feat: update index

--- src/main/java/br/dev/zancanela/quickcup_api/controller/admin/IndexController.java
+++ src/main/java/br/dev/zancanela/quickcup_api/controller/admin/IndexController.java
@@ -1,10 +1,14 @@
 package br.dev.zancanela.quickcup_api.controller.admin;
 
 import br.dev.zancanela.quickcup_api.util.ApiConstants;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static br.dev.zancanela.quickcup_api.util.ApiConstants.MV_OBJECT_CURRENT_PAGE;
 
@@ -12,10 +16,30 @@ import static br.dev.zancanela.quickcup_api.util.ApiConstants.MV_OBJECT_CURRENT_
 @RequestMapping("/")
 public class IndexController {
 
+    @Value("${security.api.key}")
+    private String securityApiKey;
+
+    @Value("${quickcup.users.default.password}")
+    private String defaultPassword;
+
+    @Value("${quickcup.link.formulario.testes}")
+    private String linkFormularioTestes;
+
+    @Value("${quickcup.link.aplicacao.cliente}")
+    private String linkAplicacaoCliente;
+
     @GetMapping()
     public ModelAndView index() {
 
         ModelAndView mv = new ModelAndView(ApiConstants.VIEW_INDEX);
+
+        Map<String, String> indexVariables = new HashMap<>();
+        indexVariables.put("apiKey", securityApiKey);
+        indexVariables.put("defaultPassword", defaultPassword);
+        indexVariables.put("linkFormularioTestes", linkFormularioTestes);
+        indexVariables.put("linkAplicacaoCliente", linkAplicacaoCliente);
+
+        mv.addAllObjects(indexVariables);
 
         mv.addObject(MV_OBJECT_CURRENT_PAGE, "index");
 
