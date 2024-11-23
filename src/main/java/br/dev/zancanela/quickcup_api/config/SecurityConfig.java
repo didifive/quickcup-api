e64edb562f4caea2789542464e16b79d7ea0de08
Luis Zancanela <luisczdidi@gmail.com>
2024-11-23 19:05:31 -0300

    feat: update error handling

--- src/main/java/br/dev/zancanela/quickcup_api/config/SecurityConfig.java
+++ src/main/java/br/dev/zancanela/quickcup_api/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package br.dev.zancanela.quickcup_api.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,7 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -101,12 +100,14 @@ public class SecurityConfig {
                         .deleteCookies("JSESSIONID"))
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/api/v1/**"))
                 .exceptionHandling(exception -> exception
-                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-                        .accessDeniedHandler((request, response, accessDeniedException) -> response.sendRedirect(ERROR_PATH))
-                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.FORBIDDEN))
-                        .accessDeniedHandler((request, response, accessDeniedException) -> response.sendRedirect(ERROR_PATH))
+                        .accessDeniedHandler(accessDeniedHandler())
                 );
         return httpSecurity.build();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, accessDeniedException) -> response.sendRedirect(ERROR_PATH);
     }
 
     @Bean
